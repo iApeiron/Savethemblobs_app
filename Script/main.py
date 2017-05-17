38184facf78b55ba89a727be7b1fc08d6085f20c
iApeiron <iApeiron@users.noreply.github.com>
2017-05-17 10:52:05 -0700

    now recommends --help

--- Script/main.py
+++ Script/main.py
@@ -15,7 +15,7 @@ except NameError:
 savethemblobs = sys.argv[1] if sys.argv[1:] else 'savethemblobs.py'
 ecid = input("Enter ECID ").strip() or '1050808663311'
 device = input("Enter model (eg. iPhone3,1 or n90ap) ").strip() or 'iPhone3,1'
-opt = input("Optional arg (eg. --cydia-blobs) ").strip() or '--overwrite-apple'
+opt = input("Optional arg (eg. --help) ").strip() or '--overwrite-apple'
 fmt = "%s: savethemblobs %s %s %s"
 print((fmt % (sys.argv[0].split('/')[-1], ecid, device, opt)))
 sys.argv = ['savethemblobs.py', ecid, device, opt]
